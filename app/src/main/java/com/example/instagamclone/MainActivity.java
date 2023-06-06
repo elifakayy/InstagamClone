46852ffa6eb30cd070041b2a6237aa0cd47610a6
eelif <eelifaakay@gmail.com>
2023-06-06 16:36:01 +0300

    Instagram clone start, firebase and ui

---
+++ app/src/main/java/com/example/instagamclone/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.instagamclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void signinClicked(View view) {
+
+    }
+
+    public void signupClicked(View view) {
+
+    }
+
+    public void uploadClicked(View view)
+    {
+
+    }
+
+    public void imageClicked(View view)
+    {
+        
+    }
+}
+
